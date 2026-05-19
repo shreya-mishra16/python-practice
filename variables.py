70ae12c27395c1f272f66ec42f8452cfc784da4f
shreya-mishra16 <shreyamishra9989@gmail.com>
2026-05-19 07:27:26 +0530

    Uploaded Python variable practice questions

---
+++ variables.py
@@ -0,0 +1,96 @@
+#Q-1 Sum of two numbers
+a = 10
+b = 20
+sum = a+b
+print(sum)
+
+# #Q-2 print Username
+name = str(input("Enter your name:"))
+print("Hello",name)
+
+#Q-3 Area of rectangle
+length = 5
+breadth = 6
+area = length*breadth
+print(area)
+
+# #Q-4 Area of Circle
+radius = 7
+pi = 3.14
+area = pi*radius*radius
+print(area)
+
+#Q-5 Swap two variables3
+
+a = 5
+b = 10
+a,b = b,a
+print(a)
+print(b)
+
+#Q-6 Celsius to Fahrenhit
+
+celsius = 25
+fahrenhit = (9/5*celsius)+32
+print(fahrenhit)
+
+#Q-7 Simple Interest
+
+p  = 1000
+r = 5
+t = 2
+
+si = (p*r*t)/100
+print(si)
+
+#Q-8 Avearge of three numbers
+
+a=10
+b=20
+c=30
+average = (a+b+c)/3
+print(average)
+
+#Q-9 Square of a number
+
+num = 6
+square = num**2
+print(square)
+
+
+#Q-10 Cube of a number
+num = 2
+cube = num**3
+print(cube)
+
+#Q-11 Age AFter 5 years
+age = 18
+future_age = age+5
+print(future_age)
+
+#Q-12 Kilometer to meter conversion
+
+km = 5
+meter = km*1000
+print(meter)
+
+#Q-13 Student Details
+name = "Shreya"
+age = 21
+course = "python"
+print(name)
+print(age)
+print(course)
+
+#Q-14 Total marks
+maths = 80
+science = 75
+english = 90
+total = maths + science + english
+print(total)
+
+#Q-15 Salary bonus
+
+salary = 50000
+bonus = salary*0.10
+print(bonus)
